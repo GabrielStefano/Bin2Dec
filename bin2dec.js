bb56bc5f9b3226213af925e5d8f83cec3fe533c6
Gabriel Stefano <gabriel.lrnc@gmail.com>
2021-02-16 01:19:55 -0300

    done

---
+++ bin2dec.js
@@ -0,0 +1,30 @@
+var a = document.querySelector("input#add0")
+var b = document.querySelector("input#add1")
+var c = document.querySelector("input#convert")
+var value_input = document.querySelector("input#value")
+var result = document.querySelector("input#result")
+
+a.addEventListener('click', add0)
+b.addEventListener('click', add1)
+
+c.addEventListener('click', convert)
+
+
+function add0(){
+    value_input.value = `${value_input.value}0`
+}
+
+function add1(){
+    value_input.value = `${value_input.value}1`
+}
+
+function convert(){
+
+    if(/[2-9]+/.test(value_input.value)==true){
+        window.alert('Invalid input!')
+    }
+    else{
+        var resultado = parseInt(value_input.value, 2)
+        result.value = `${resultado}`
+    }
+}
